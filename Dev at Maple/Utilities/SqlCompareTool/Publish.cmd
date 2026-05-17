25e2008c9f2d1fd4ea69114f258a12988aa4ff3d
Colin Naylor <colinnaylor@gmail.com>
2026-05-17 15:11:54 +0100

    Delete Dev at Maple/Utilities directory

--- Dev at Maple/Utilities/SqlCompareTool/Publish.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-xcopy bin\release\*.* C:\maple\SqlCompare\*.* /s /y
-echo.
-echo.
-dir C:\maple\SqlCompare\*.*
-echo.
-echo.
--- Dev at Maple/Utilities/SystemsTestTool/bin/Release/Files/RunBoss.cmd
+++
@@ -1,5 +0,0 @@
-﻿@echo off
-
-S:\Apps\robocopy.exe S:\Apps\BossUI\Live C:\Maple\BossUI\Live /MIR /W:1
-cd /d C:\Maple\BossUI\Live\
-start C:\Maple\BossUI\Live\BossUI.exe %3
